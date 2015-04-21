3e3def8802b9d2d156387ef42f68988a041c2c57
Thomas Carney <tommycarney@gmail.com>
2015-04-21 18:40:06 +0200

    adds the flash using bootstrap classes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,14 @@
 class VotesController < ApplicationController
   before_filter :authenticate_user!
   def create
-  	 @comment = current_user.votes.create(params.require(:vote).permit(:link_id, :up))
-  	 redirect_to :back
+  	@vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+  	if @vote
+  		@vote.up = params[:vote][:up]
+  		@vote.save
+  	else
+		@vote = current_user.votes.create(params.require(:vote).permit(:link_id, :up))
+	end  	 
+  	redirect_to :back
   end
+  
 end
